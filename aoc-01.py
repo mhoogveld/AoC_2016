a5688f4e5e975ef90d7e426a068b8eb1cf21e18e
Maarten Hoogveld <maarten@hoogveld.org>
2016-12-04 23:27:08 +0100

    Added part 2 of day 1

--- aoc-01.py
+++ aoc-01.py
@@ -16,19 +16,28 @@ class AoC_01:
         self.coord_x = 0
         self.coord_y = 0
         self.direction = self.North
+        self.path_walked = list()
 
     def run(self):
+        puzzle_part = 2
         for instruction in self.directions.split(','):
-            self.perform_instruction(instruction.strip())
-        return 'Distance: {}'.format(self.coord_x + self.coord_y)
+            continue_instructions = self.perform_instruction(instruction.strip(), puzzle_part)
+            if not continue_instructions:
+                break
+        print('Distance: {}'.format(self.coord_x + self.coord_y))
 
-    def perform_instruction(self, instruction):
+    def perform_instruction(self, instruction, puzzle_part):
         instr = re.match('([LR])([1-9][0-9]*)', instruction)
         if instr:
             self.turn(instr.group(1))
-            self.walk(instr.group(2))
+            if puzzle_part == 1:
+                continue_walking = self.walk(int(instr.group(2)))
+            else:
+                continue_walking = self.walk_until_cross_path(int(instr.group(2)))
         else:
+            continue_walking = False
             print("Warning: instruction {} not understood".format(instruction))
+        return continue_walking
 
     def turn(self, dir_instr):
         if dir_instr == 'L':
@@ -39,14 +48,23 @@ class AoC_01:
 
     def walk(self, dir_steps):
         if self.direction == self.North:
-            self.coord_y += int(dir_steps)
+            self.coord_y += dir_steps
         if self.direction == self.East:
-            self.coord_x += int(dir_steps)
+            self.coord_x += dir_steps
         if self.direction == self.South:
-            self.coord_y -= int(dir_steps)
+            self.coord_y -= dir_steps
         if self.direction == self.West:
-            self.coord_y -= int(dir_steps)
+            self.coord_x -= dir_steps
+        return True
 
+    def walk_until_cross_path(self, dir_steps):
+        for step in range(0, dir_steps):
+            self.walk(1)
+            pos = (self.coord_x, self.coord_y)
+            if pos in self.path_walked:
+                return False
+            self.path_walked.append(pos)
+        return True
 
 if __name__ == "__main__":
     solver = AoC_01()
